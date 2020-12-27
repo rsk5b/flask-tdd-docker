38e28e2b0f74364edfb0252f12f66a8e1fcc27f7
rsk5b <randall.s.kelley@gmail.com>
2020-12-27 12:36:26 -0500

    fixed swagger in prod

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ import os
 from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 # instantiate the db
 db = SQLAlchemy(session_options={"autocommit": False})
@@ -19,6 +20,7 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
