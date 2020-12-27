243696f7a62a1eb69b4ef6fa03b45fbcdbfab2e3
rsk5b <randall.s.kelley@gmail.com>
2020-12-26 21:36:48 -0500

    added secret key as an env variable

--- src/config.py
+++ src/config.py
@@ -20,6 +20,7 @@ class TestingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
 
 
 class LocalConfig(BaseConfig):
